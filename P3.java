af33a81dc69e526ed178fa95de57ce2c438fb355
shree <sammmmmu123@gmail.com>
2022-07-20 22:25:06 +0530

    changed sorting fun

--- P3.java
+++ P3.java
@@ -11,7 +11,19 @@ public class P3 {
             System.out.println("Enter " + i + " th number: ");
             arr[i] = inp.nextInt();
         }
-        Arrays.sort(arr);
+        Boolean changed = true;
+        // bubble sort
+        while (changed) {
+            changed = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    changed = true;
+                }
+            }
+        }
         System.out.println("Sorted array: " + Arrays.toString(arr));
         inp.close();
     }
